db03637646b7467f9334a0caf17a99eb1bd3c82d
tamimdevlopment <ha2020anan@gmail.com>
2026-09-12 21:07:12 +0300

    Update __init__.py

--- delus/__init__.py
+++ delus/__init__.py
@@ -1,7 +1,6 @@
-from .c.delus_main_file_py_to_c import Mylang as C
-from .asm.delus_main_file_py_to_asm import mylang as ASM
-from .docker.delus_main_file_py_to_docker import Docker as Docker
-from .html.delus_main_file_py_to_html import Html as HTML
+import .c
+import .asm
+import .docker
+import .html
 
 
-__all__ = ["C", "ASM", "Docker", "HTML"]
